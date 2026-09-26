a351c507cf265fc4335258e7efb3ef54b2279e06
sojkajaroslav <sojka.jaroslav@gmail.com>
2026-09-26 17:33:33 +0200

    Zrychlit mobilní vykreslení a optimalizovat obrázky

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,2 @@
 import { defineConfig } from 'astro/config';
-export default defineConfig({ site: 'https://www.binisi.cz', output: 'static' });
+export default defineConfig({ site: 'https://www.binisi.cz', output: 'static', build: { inlineStylesheets: 'always' } });
